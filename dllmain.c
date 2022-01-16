6a6c3dee911e473b4d4f2a208aa0fd52edb09ce2
Untitled_unrevised <348070954@qq.com>
2022-01-17 01:13:12 +0800

    Add files via upload
    for windows

---
+++ dllmain.c
@@ -0,0 +1,26 @@
+#include "dll.h"
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	switch(fdwReason)
+	{
+		case DLL_PROCESS_ATTACH:
+		{
+			break;
+		}
+		case DLL_PROCESS_DETACH:
+		{
+			break;
+		}
+		case DLL_THREAD_ATTACH:
+		{
+			break;
+		}
+		case DLL_THREAD_DETACH:
+		{
+			break;
+		}
+	}
+	
+	return TRUE;
+}
